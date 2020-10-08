1da4c6b29843fc9ae4486935557549e50a83dbda
Jessica Nguyen <jessicaawhy@gmail.com>
2020-10-08 13:04:50 -0700

    Create header module

---
+++ src/header.js
@@ -0,0 +1,19 @@
+import {createAndAppend, setElementHtmlText} from './createElement'
+
+function createHeader() {
+  createAndAppend('h1', 'restaurantName', 'content');
+  setElementHtmlText('restaurantName', 'Yummy Yummy')
+  
+  createAndAppend('ul', 'linkContainer', 'content');
+  
+  createAndAppend('li', 'home', 'linkContainer');
+  setElementHtmlText('home', 'Home');
+  
+  createAndAppend('li', 'menu', 'linkContainer');
+  setElementHtmlText('menu', 'Menu');
+  
+  createAndAppend('li', 'contact', 'linkContainer');
+  setElementHtmlText('contact', 'Contact');
+}
+
+export {createHeader}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-import {createAndAppend, setElementHtmlText} from './createElement'
+import {createHeader} from './header'
 
-createAndAppend('h1', 'restaurantName', 'content');
-setElementHtmlText('restaurantName', 'Yummy Yummy')
-
-
-
-
-
-// function createAndAppend(type, id, parentId) {
-//   const element = document.createElement(`${type}`);
-//   element.setAttribute('id', id);
-
-//   const parentElement = document.getElementById(parentId);
-//   parentElement.appendChild(element);
-// }
-
-// function setElementHtmlText(id, text) {
-//   document.getElementById(id).textContent = text;
-// }
+createHeader()
