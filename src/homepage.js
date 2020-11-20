8d06e234cde264a974fee3c94d84802d91b022f5
Jessica Nguyen <jessicaawhy@gmail.com>
2020-11-19 21:17:44 -0800

    Move slider images to separate module

--- src/homepage.js
+++ src/homepage.js
@@ -1,32 +1,4 @@
-const images = {
-  1: {
-    text: 'WE ARE DEVOTED <br>TO CRAFTING MOMENTS <br>THAT TRANSCEND',
-  },
-
-  2: {
-    text: 'AND HOLD THE POWER <br>TO EVOKE LASTING MEMORIES.',
-  },
-
-  3: {
-    text: 'IN EVERY TECHNIQUE <br>AND INGREDIENT <br>WE HONOR THE PAST <br>AND CONTRIBUTE TO',
-  },
-
-  4: {
-    text: 'THE EVOLUTION OF A <br>SACRED TRADITION',
-  },
-
-  5: {
-    text: 'ONE CHEF AND FOUR GUESTS,',
-  },
-
-  6: {
-    text: 'FORGE A BOND AND EXPERIENCE',
-  },
-
-  7: {
-    text: 'THE INTIMATE CONNECTION <br>BETWEEN EARTH AND SEA.',
-  },
-}
+import { images } from './images';
 
 let currImgIndex = 1;
 let intervalId;
