2599dbed4cfccd4ca27e51634ebf41966a21628f
Jessica Nguyen <jessicaawhy@gmail.com>
2021-02-05 21:00:25 -0800

    Add embedded map and minor styling changes

--- src/location.js
+++ src/location.js
@@ -67,6 +67,17 @@ function createLocationPage() {
       parentElement: mainContainer,
     });
   });
+
+  const locationMap = create({
+    type: 'iframe', 
+    innerHTML: '',
+    attributes: {
+      src: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d100895.87157766047!2d-122.42357736657523!3d37.79013381956145!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808580b1c8cede0b%3A0xc7934c15cbf08de1!2sJu-Ni!5e0!3m2!1sen!2sus!4v1612587120074!5m2!1sen!2sus"
+    },
+    classList: [],
+    parentElement: mainContainer,
+  });
+
 }
 
 export { createLocationPage };
